aa0e4e7ba7de18a62eada98f3fda2f24d714b89c
Emerson Estrella <emerson.estrella@gmail.com>
2012-05-06 20:45:47 -0300

    fix

--- js/game.js
+++ js/game.js
@@ -115,7 +115,7 @@ Game.prototype.init = function(){
   this.draw_bg();
   
   this.remaining_time = this.num_pieces*30;
-  this.time_to_complete = this.num_pieces*30;
+  this.time_to_complete = this.remaining_time;
   this.clock_interval = null;
   this.mouse = new Mouse(this);
 
