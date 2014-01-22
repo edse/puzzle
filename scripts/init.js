d9fd557ecee7eaa940684e99e780e17121ec15a3
Emerson Estrella <emerson.estrella@gmail.com>
2014-01-22 03:31:11 -0200

    ...

---
+++ scripts/init.js
@@ -0,0 +1,278 @@
+// polyfill for animation frame
+( function() {
+  var lastTime = 0;
+  var vendors = ['ms', 'moz', 'webkit', 'o'];
+  for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+  }
+  if(!window.requestAnimationFrame) {
+    console.log('!window.requestAnimationFrame');
+    window.requestAnimationFrame = function(callback, element) {
+      var currTime = new Date().getTime();
+      var timeToCall = Math.max(0, 22 - (currTime - lastTime));
+      var id = window.setTimeout(function() { callback(currTime + timeToCall);
+      }, timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+  }
+  if(!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = function(id) {
+      clearTimeout(id);
+    };
+  }
+}());
+
+if(Modernizr.fullscreen){
+  function RunPrefixMethod(obj, method) {
+    var pfx = ["webkit", "moz", "ms", "o", ""];
+    var p = 0, m, t;
+    while (p < pfx.length && !obj[m]) {
+      m = method;
+      if (pfx[p] == "") {
+        m = m.substr(0,1).toLowerCase() + m.substr(1);
+      }
+      m = pfx[p] + m;
+      t = typeof obj[m];
+      if (t != "undefined") {
+        pfx = [pfx[p]];
+        return (t == "function" ? obj[m]() : obj[m]);
+      }
+      p++;
+    }
+  }
+}
+
+// GAME START
+var game = new Game();
+var interval = null;
+var gameInterval = null;
+game.debug = false;
+m = {
+  game : game
+};
+
+interv = function() {
+  interval = setTimeout("game.mouse.moving = false; document.getElementById('moving').value = false; intervClear();", 500);
+};
+intervClear = function() {
+  clearInterval(interval)
+};
+
+stopGame = function() {
+  clearInterval(gameInterval);
+  
+  game.started = false;
+  stopSFX();
+  stopBGM();
+  window.cancelAnimationFrame(game.interval);
+
+  $('#home').addClass('active');
+
+  $('#play').show();
+  $('.control').hide();
+  
+  $('#canvas, #canvas_bg').hide();
+  $('.content').show();
+};
+
+startGame = function() {
+  clearInterval(gameInterval);
+  gameInterval = setInterval(function() { game.remaining_time--; },1000);
+  game.started = true;
+  //resizeGame();
+  startSFX();
+  startBGM();
+  loop();
+  $('#home').removeClass('active');
+  $('#canvas, .control, .abs').show();
+  $('.content, #play, #exitfullscreen, #bgm, #sfx, #autosnap').hide();
+  $('.container, .footer').hide();
+  $('#body').css('padding', '0px');
+  $('#body').css('margin', '0px');
+};
+
+pauseGame = function() {
+  clearInterval(gameInterval);
+  game.started = false;
+  window.cancelAnimationFrame(game.interval);
+
+  $('.control').hide();  
+  $('#play').show();
+  $('#btn-play').show();
+};
+
+stopSFX = function() {
+  game.drip.volume = 0.0;
+  game.twang.volume = 0.0;
+  game.drip.pause();
+  game.twang.pause();
+  $('#sfxoff').hide();
+  $('#sfx').show();
+};
+
+startSFX = function() {
+  game.drip.volume = 1.0;
+  game.twang.volume = 1.0;
+  $('#sfxoff').show();
+  $('#sfx').hide();
+};
+
+stopBGM = function() {
+  game.bgm.volume = 0.0;
+  game.bgm.pause();
+  $('#bgmoff').hide();
+  $('#bgm').show();
+};
+
+startBGM = function() {
+  game.bgm.volume = 1.0;
+  game.bgm.play();
+  //game.bgm.start(0);
+  //document.getElementById('audio-bgm').play();
+  $('#bgmoff').show();
+  $('#bgm').hide();
+};
+
+autoSnap = function() {
+  game.auto_snap = true;
+  $('#autosnapoff').show();
+  $('#autosnap').hide();
+};
+
+autoSnapOff = function() {
+  game.auto_snap = false;
+  $('#autosnapoff').hide();
+  $('#autosnap').show();
+};
+
+fullscreen = function() {
+  RunPrefixMethod(game.canvas, "RequestFullScreen");
+};
+
+exitfullscreen = function() {
+  RunPrefixMethod(document, 'CancelFullScreen');
+};
+
+function start() {
+  startGame();
+}
+function stop() {
+  stopGame();
+}
+function pause() {
+  pauseGame();
+}
+
+function loop() {
+  game.interval = window.requestAnimationFrame(loop, game.canvas);
+
+  game.render();
+
+  var elapsed = game.getTimer() - game.time;
+  game.time = game.getTimer();
+  if(elapsed > game.maxElapsedTime)
+    game.maxElapsedTime = elapsed;
+}
+
+function loadAssets(g,assets) {
+  for(i=0; i<assets.length; i++){
+    if(assets[i].type == "image"){
+      //IMAGE
+      eval("g."+assets[i].slug+' = new Image();');
+      eval("g."+assets[i].slug+'.src = "'+assets[i].src+'";');
+      eval("g."+assets[i].slug+'.onload = g.loaded_items++;');
+    }
+    else if(assets[i].type == "audio"){
+      //AUDIO
+      eval("g."+assets[i].slug+' = document.createElement(\'audio\');');
+      eval("g."+assets[i].slug+'.addEventListener(\'canplaythrough\', itemLoaded(g), false);');
+      var source= document.createElement('source');
+      if(Modernizr.audio.ogg){
+        source.type= 'audio/ogg';
+        source.src= assets[i].src+'.ogg';
+      }
+      else if(Modernizr.audio.mp3){
+        source.type= 'audio/mpeg';
+        source.src= assets[i].src+'.mp3';
+      }
+      if(source.src != ""){
+        eval("g."+assets[i].slug+'.appendChild(source);');
+      }
+      else{
+        // no MP3 or OGG audio support
+        g.itens_to_load--;
+      }
+    }
+  }
+}
+
+function itemLoaded(g) {
+  g.loaded_items++;
+}
+
+function resizeGame() {  
+  location.reload();
+}
+window.addEventListener('resize', resizeGame, false);
+window.addEventListener('orientationchange', resizeGame, false);
+//
+
+$(function() {  
+  $(".popover-test").popover();
+  
+  $("#next").click(function() {
+    game.nextStage();
+    $('#modal-success').modal('hide');
+  });
+  
+  $("#play, #btn-play, #play-btn-lg").click(function() {
+    start();
+  });
+  
+  $("#btn-pause").click(function() {
+    pause();
+  });
+  
+  $("#btn-fullscreen").click(function() {
+    fullscreen();
+  });
+  
+  $("#btn-exitfullscreen").click(function() {
+    exitfullscreen();
+  });
+  
+  $("#btn-bmg-on").click(function() {
+    startBGM();
+  });
+  
+  $("#btn-bmg-off").click(function() {
+    stopBGM();
+  });
+  
+  $("#btn-sfx-on").click(function() {
+    startSFX();
+  });
+  
+  $("#btn-sfx-off").click(function() {
+    stopSFX();
+  });
+  
+  $("#btn-autosnap-on").click(function() {
+    autoSnap();
+  });
+  
+  $("#btn-autosnap-off").click(function() {
+    autoSnapOff();
+  });
+
+  $("#play-top").click(function() {
+    start();
+  });
+
+  $("#btn-home").click(function() {
+    self.location.href="./index.html";
+  });
+  
+});
---
+++ scripts/piece.js
@@ -0,0 +1,180 @@
+/*****
+ *
+ *   holder.js
+ *
+ *****/
+
+/*****
+ *
+ *   constructor
+ *
+ *****/
+function Piece(id, game, width, height, x, y, startPoint, target, holder, moveble, placed) {
+  if(arguments.length > 0) {
+    this.id = id;
+    this.game = game;
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.target = target;
+    this.startPoint = new Point2D(target.x,target.y),
+    this.holder = holder;
+    this.moveble = moveble;
+    this.placed = placed;
+    this.moveble = false;
+    this.m = (target.y - this.y)/(target.x - this.x);
+    this.b = target.y - (this.m * target.x);
+    if(Math.random() >= 0.5)
+      this.p = 0.1;
+    else
+      this.p = -0.1;
+  }
+  else{
+    this.id = 0;
+    this.game = null;
+    this.width = 10;
+    this.height = 10;
+    this.x = 0;
+    this.y = 0;
+    this.target = null;
+    this.startPoint = null;
+    this.holder = null;
+    this.moveble = false;
+    this.placed = null;
+  }
+  this.tolerance = 200;
+  this.moving = false;
+  this.placed = false;
+}
+
+
+Piece.prototype.draw = function() {
+  if((!this.moveble)&&(!this.placed)){
+    this.game.context.globalAlpha = 1
+    //this.game.context.clearRect(0, 0, game.canvas.width, game.canvas.height);
+    //this.game.context.clearRect(Math.round(this.startPoint.x-this.game.piece_width/2), Math.round(this.startPoint.y-this.game.piece_height/2), this.game.piece_width, this.game.piece_height);
+    //this.game.context.fillRect(Math.round(this.startPoint.x-this.game.piece_width/2), Math.round(this.startPoint.y-this.game.piece_height/2), this.game.piece_width, this.game.piece_height);
+    
+    //this.game.context.drawImage(this.game.context_bg.getImageData(Math.round(this.startPoint.x-this.game.piece_width/2), Math.round(this.startPoint.y-this.game.piece_height/2), this.game.piece_width, this.game.piece_height), Math.round(this.startPoint.x-this.game.piece_width/2), Math.round(this.startPoint.y-this.game.piece_height/2), this.game.piece_width, this.game.piece_height, Math.round(this.startPoint.x-this.game.piece_width/2), Math.round(this.startPoint.y-this.game.piece_height/2), this.game.piece_width, this.game.piece_height );
+    //this.game.context.putImageData(this.game.context_bg.getImageData(Math.round(this.startPoint.x-this.game.piece_width/2), Math.round(this.startPoint.y-this.game.piece_height/2), this.game.piece_width, this.game.piece_height), Math.round(this.startPoint.x-this.game.piece_width/2), Math.round(this.startPoint.y-this.game.piece_height/2));
+    
+    this.p = this.p*1.1;
+    this.startPoint.x = this.startPoint.x + this.p;
+    this.startPoint.y = this.m * this.startPoint.x + this.b;
+    /*
+    if((this.startPoint.x > (this.game.canvas.width/this.scale)-this.game.img.width/this.game.num_lines) || 
+      (this.startPoint.y > (this.game.canvas.height/this.scale)-this.game.img.height/this.game.num_lines) || 
+      (this.startPoint.x < this.game.img.width/this.game.num_lines) || 
+      (this.startPoint.y < this.game.img.height/this.game.num_lines)){
+    */
+    if((this.startPoint.x+this.width/2 >= (this.game.canvas.width/this.game.scale)) || 
+      (this.startPoint.y+this.height/2 >= (this.game.canvas.height/this.game.scale)) || 
+      (this.startPoint.x-this.width/2 <= 0) || 
+      (this.startPoint.y-this.height/2 <= 60)){
+      this.moveble = true;
+      this.x = this.startPoint.x;
+      this.y = this.startPoint.y;
+      this.iniPoint = new Point2D(this.x,this.y)
+    }
+   
+    //this.game.context.save();
+    //this.game.context.globalAlpha = 1;
+    //this.game.context.beginPath();
+    
+    //this.game.context.putImageData(this.game.context_bg.getImageData(Math.round(this.startPoint.x-this.game.piece_width/2), Math.round(this.startPoint.y-this.game.piece_height/2), this.game.piece_width, this.game.piece_height), Math.round(this.startPoint.x-this.game.piece_width/2), Math.round(this.startPoint.y-this.game.piece_height/2));
+    //this.game.context.globalAlpha = 1
+    this.game.context.drawImage(this.game.img, this.holder.column*this.game.piece_width, this.holder.line*this.game.piece_height, this.game.piece_width, this.game.piece_height, 
+      this.startPoint.x-this.game.piece_width/2, this.startPoint.y-this.game.piece_height/2, this.game.piece_width, this.game.piece_height);
+
+  }
+  else{
+    //this.game.context.save();
+    
+    if(this.placed)
+      this.game.context.globalAlpha = 1
+    else if(!this.game.is_over)
+      this.game.context.globalAlpha = 0.8
+    else
+      this.game.context.globalAlpha = 1
+  
+    this.game.context.fillStyle = "rgba(255, 255, 255, 0.5)";
+    //over = this.game.mouse.isOverPiece(this);
+  
+    /*  
+    if(!this.game.selected){
+      if((!this.game.over)||(this.game.over.id < this.id)||(this.mouse_is_over())){
+        if(this.mouse_is_over()){
+          this.game.over = this;
+        }
+      }
+    }
+    */
+  
+    if(this == this.game.selected){
+      this.game.context.fillStyle = "rgba(0, 0, 255, 0.1)";
+    }
+    else if(this.game.over == this)
+      this.game.context.fillStyle = "rgba(255, 0, 0, 0.1)";
+  
+    //target distance
+    if((this.game.selected == this)&&(this.near())){
+      this.game.context.fillStyle = "rgba(0, 255, 0, 0.1)";
+      if((this.game.auto_snap == true)&&(!this.placed)){
+        //place
+        this.game.selected.x = this.game.selected.target.x;
+        this.game.selected.y = this.game.selected.target.y;
+        this.game.selected.placed = true;
+        this.game.selected.moveble = false;
+        this.game.placed_pieces.push(this.game.selected);
+        if(this.game.drip.currentTime != 0)
+          this.game.drip.currentTime = 0;
+        this.game.drip.play();
+      }
+    }
+  
+    //piece.draw();
+    //this.game.context.beginPath();
+    //this.game.context.fillRect(Math.round(this.startPoint.x-this.game.piece_width/2), Math.round(this.startPoint.y-this.game.piece_height/2), this.game.piece_width, this.game.piece_height);
+    /*
+    this.game.context.drawImage(this.game.img, this.holder.column*this.game.piece_width, this.holder.line*this.game.piece_height, this.game.piece_width, this.game.piece_height, 
+      Math.round(this.x-this.game.piece_width/2), Math.round(this.y-this.game.piece_height/2), this.game.piece_width, this.game.piece_height);
+    */
+    this.game.context.drawImage(this.game.img, this.holder.column*this.game.piece_width, this.holder.line*this.game.piece_height, this.game.piece_width, this.game.piece_height, 
+      this.x-this.game.piece_width/2, this.y-this.game.piece_height/2, this.game.piece_width, this.game.piece_height);
+
+  
+    if(!this.game.is_over){
+      //this.game.context.strokeRect(this.x-this.width/2,this.y-this.height/2,this.width,this.height);
+      //this.game.context.fillRect(this.x-this.width/2,this.y-this.height/2,this.width,this.height);
+      //this.game.context.globalAlpha = 1
+      //this.game.context.fillStyle = "rgba(0, 0, 0, 1)";
+      //this.game.context.fillText(this.id, this.x-3, this.y+3);
+    }
+    
+    //this.game.context.closePath();
+    //this.game.context.restore();
+  }
+  
+  if(this.game.debug)
+    console.log('pieace: '+this.id+' drew');
+}
+
+Piece.prototype.near = function() {
+  //target distance
+  var r = false
+  var dx = this.x - this.target.x;
+  var dy = this.y - this.target.y;
+  var distance = (dx * dx + dy * dy);
+  if(distance <= this.tolerance){
+    r = true;
+  }
+  if(this.game.debug){
+    console.log(this.id+': '+distance)
+  }
+  return r;
+}
+
+Piece.prototype.mouse_is_over = function() {
+  return this.game.mouse.isOverPiece(this);
+}
